a9da535031ed79102d51db63b9da68eaf276d8d0
Arif Ali <arif.ali@canonical.com>
2020-06-10 12:04:45 +0000

    Add a line to unount all the snap loop devices

--- scripts/run_job.sh
+++ scripts/run_job.sh
@@ -2,7 +2,11 @@
 
 . CONFIG
 
+# stop unnecessary services
 sudo systemctl stop ${SERVICES}
+
+# Unmount all the snap loop devices
+for mount in $(df -h | grep loop.*snap | awk '{print $6}'); do sudo umount $mount; done
 
 cd ${SCRIPTSDIR}/../configs
 ${WORKDIR}/hpl-2.3/bin/rpi4-mpich/xhpl
